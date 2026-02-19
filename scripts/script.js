aa2977153ce6df43e456e510176840bc0f75ad6a
WujecDamian <wujecdamian12@gmail.com>
2026-02-20 00:14:10 +0100

    feat(script): add addBookToLibrary function

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 const myLibrary = []
+const titleInput = document.querySelector('#title')
+const authorInput = document.querySelector('#author')
+const pagesInput = document.querySelector('#pages')
+const submitBtn = document.querySelector('#submit-btn')
 
-function book (title, author, pages, read) {
+function Book (title, author, pages, read) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor")
   }
@@ -23,10 +27,16 @@ function book (title, author, pages, read) {
     )
   }
 }
-const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien', 295, false)
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
 console.log(theHobbit.info())
 
+submitBtn.addEventListener('click', addBookToLibrary)
 function addBookToLibrary () {
   // take params, create a book then store it in the array
-  let title = ''
+  let title = titleInput.value
+  let author = authorInput.value
+  let pages = pagesInput.value
+  const book1 = new Book(title, author, pages, false)
+  myLibrary.push(book1)
 }
+console.table(myLibrary)
