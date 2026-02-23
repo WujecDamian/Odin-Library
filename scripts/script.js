5c322d88d515d5136e62b03eb61d81ba7518cc8f
WujecDamian <wujecdamian12@gmail.com>
2026-02-23 19:35:10 +0100

    refactor: convert Book to class syntax

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ const authorInput = document.querySelector('#author')
 const pagesInput = document.querySelector('#pages')
 const submitBtn = document.querySelector('#submit-btn')
 const booksSection = document.querySelector('.books')
-function Book (id, title, author, pages, read) {
+/* function Book (id, title, author, pages, read) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor")
   }
@@ -26,6 +26,30 @@ function Book (id, title, author, pages, read) {
         this.pages
       } pages, ${this.haveRead()} `
     )
+  }
+} */
+/* convert to class */
+class Book {
+  constructor (id, title, author, pages, read) {
+    this.id = id
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+  }
+  info () {
+    console.log(
+      `${this.title} by ${this.author}, ${
+        this.pages
+      } pages, ${this.haveRead()} `
+    )
+  }
+  haveRead () {
+    if (this.read) {
+      return 'already read it'
+    } else {
+      return 'not read yet'
+    }
   }
 }
 /* add books manually (starting array) */
